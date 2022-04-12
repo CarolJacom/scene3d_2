7488ccd051ca558ccc956e61eeb80fd8c03fb641
Caroline Jacomini <caroljacom@gmail.com>
2022-04-12 17:06:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ function createFreeCamera(scene) {
   camera.keysRight.push('D'.charCodeAt(0));
 
   camera.position = new BABYLON.Vector3(6.5, 1.7, -22);
-  camera.rotation = new BABYLON.Vector3( 0, -0.7, 0);
+  camera.rotation = new BABYLON.Vector3( 0, -0.9, 0);
   
   return camera;
 }
